e138424bd519d884ee04928fe523137bf21608ff
saivenkat900 <99874195+saivenkat900@users.noreply.github.com>
2022-02-18 15:19:24 +0530

    Add files via upload

---
+++ sai venkatt/sai.py
@@ -0,0 +1,25 @@
+# n={1,2,3,4}
+# print(n)
+# print(n,type(n))
+# a={1,2,3};b={4,5,6}
+# print(a.intersection(b))
+# a={2,4,5};b={4,5,6}
+# print(a.intersection(b))
+# a={3,4,5,7}
+# b={3,6,5,8}
+# print(a.difference(b))
+# print(b.difference(a))
+# print(a-b)
+# print(b-a)
+# v={25,65,56,89}
+# v.pop()
+# print(v)
+# d={1,3,4}
+# print(d,type(d))
+# k=frozenset(d)
+# print(k)
+# print(k[0])
+z={55,56,57,73,45,23}
+z.discard(4)
+
+
---
+++ sai venkatt/set.py
@@ -0,0 +1,30 @@
+# a={1,2,3};b={2,3,4};c={8,9,10};d={9,10,11}
+# print(a.intersection(b))
+# print(a.intersection(c))
+# a={1,2,3};b={3,4,5}
+# print(a.union(b))
+# a={1,2,3}
+# b={4,3,5}
+# c={4,3,7}
+# a.update(b)
+# print(a)
+# c.update(a)
+# print(c)
+# a.update(c)
+# print(c)
+# a={5,10,20,25,30}
+# b={10,21,3}
+# print(a.difference(b))
+# print(b.difference(a))
+# z={55,56,57,73,45,23}
+# z.discard(4)
+# print(z)
+# z.discard(56)
+# print(z)
+# a={1,2,3,4}
+# b={5,6,7}
+# c={4,5,6}
+# print('are a and b disjoint?',a.isdisjoint(b))
+# print('are a and c disjoint?',a.isdisjoint(c))
+# print('are b and c disjoint?',b.isdisjoint(c))
+print(dir(set))
