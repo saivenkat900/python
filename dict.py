8a40f018f48c5c8f4b27a6769b36f5eedcd90ed1
saivenkat900 <99874195+saivenkat900@users.noreply.github.com>
2022-02-20 00:35:56 +0530

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,42 @@
+# b={
+# 1: "python programing",
+# 2: "hello world",
+# 3: "advance core python"
+# }
+# print(b[1])
+# print(b[3])
+# print(b[4])
+c={1:"",2: "python",3: [1,6],4: {5,8},
+5: (7,8),6: 5,7: {3: "hi"}}
+# print(c[1],type(c[1]))
+# print(c[2],type(c[2]))
+# print(c,type(c))
+# print(c.keys())
+print(c.items())
+# c.clear()
+# print(c)
+# b=[10,11,12,13,14,'sai']
+# c=dict.fromkeys(b)
+# print(b,type(b))
+# print(dict.fromkeys(b,'hi'))
+# d={1:"",2: "python",3: [1,6],4: {5,8},
+# 5: (7,8),6: 5,7: {3: "hi"}}
+# print(d.get(2))
+# print(d[2])
+# d.pop(2)
+# print(d)
+# d.popitem()
+# print(d)
+# b={1:"",2: "python",3: [1,6],4: {5,8},
+# 5: (7,8),6: 5,7: {3: "hi"}}
+# b.update({6: "sai"})
+# print(b)
+# b.update({9: "dady"})
+# # print(b)
+# n={1:"",2: "python",3: [1,6],4: {5,8},
+# 5: (7,8),6: 5,7: {3: "hi"}}
+# n.setdefault(7, 'he')
+# n.setdefault(8, 'hello')
+# print(n)
+# print(dir(dict))
+
