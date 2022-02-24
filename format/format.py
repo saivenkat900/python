f544bddda34015feb5e8bd0359290243ecd9eb7b
saivenkat900 <99874195+saivenkat900@users.noreply.github.com>
2022-02-24 12:52:47 +0530

    Add files via upload

---
+++ format/format.py
@@ -0,0 +1,14 @@
+# a=5
+# print(a)
+# print('the value of a is %d'%a)
+# b=8
+# print(b)
+# print('%d'%b)
+# print('%f'%b)
+# print('%1f'%b)
+# c=2v 
+# print(c)
+# print(c,type(c))
+# print('%f'%c)
+# print('%.f'%c)
+# print('%.1f'%c)
