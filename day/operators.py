59798dd05f1897eeb148efe1b2f21b0bde2e4d44
saivenkat900 <99874195+saivenkat900@users.noreply.github.com>
2022-02-27 23:37:13 +0530

    Add files via upload

---
+++ day/operators.py
@@ -0,0 +1,47 @@
+# a=4//2
+# print(a)
+# b=9//3
+# print(b)
+# c=4.0//2
+# a=2; b=3
+# print(a+b)
+# a=5
+# a=a+a
+# print(a)
+# a=6
+# a+=6
+# print(a)
+# a-=2
+# print(a)
+# a*=3
+# print(a)
+# a/=2
+# print(a)
+# a**=2
+# print(a)
+# identity operator
+# a=[5]; b=5; c=6; d=[5];  e=5; f=c; g=a
+# print(a is b)
+# print(b is e)
+# print(a is g)
+# print(c is f)
+# print(b is f)
+# membership operator
+# name=['sai','hi','krish','venkat','akhil','satish','nikhil','teja','samsung','oneplus','realme','vivo','mi']
+# print('sai' in name)
+# print('apple' in name)
+# print('krishna' in name)
+# name=['s','d','r','f','t']
+# name.sort()
+# print(*name)
+# shift operator 
+# a=20
+# print(a>>1)
+# print(a>>2)
+# print(a>>3)
+# b=4
+# print(b<<1)
+# print(b<<2)
+# print(b<<3)
+
+
