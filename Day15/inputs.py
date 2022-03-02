797dce292837d34f004c98963b9029dbd1f436df
saivenkat900 <99874195+saivenkat900@users.noreply.github.com>
2022-03-02 23:42:31 +0530

    Add files via upload

---
+++ Day15/inputs.py
@@ -0,0 +1,16 @@
+# name=input("Enter your Name:")
+# age=int(input("Enter your Age:"))
+# per=float(input("Enter your Graduation percentage:"))
+# print("your Name is",name,"and age is",age,\
+#       "Graduation percentage is" ,per)
+# print("your name is"+' '+name,"and age is",age,\
+#     "graduation percentage is",str(per)+'%')
+# print("your name is %s and age is %d.\
+#     graduation percentage is %.2f"%(name,age,per))
+# print("your name is {} and age is {}. \
+#     graduation percentage is {}".format(name,age,per))
+# print("your name is {1} and age is {0}. \
+#     graduation percentage is {2}".format(age,name,per))
+# print(f"your name is {name} and age is {age}.\
+# graduation percentage is {per}")
+
