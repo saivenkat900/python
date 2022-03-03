3cdfcb771c88eb47e6950df7cdf74c66ae9104a6
saivenkat900 <99874195+saivenkat900@users.noreply.github.com>
2022-03-04 00:01:09 +0530

    Add files via upload

---
+++ str-slicing/slicing.py
@@ -0,0 +1,64 @@
+# a="saikrish"
+# print(a)
+# print(a,type(a))
+# print("saikrish"[0])#s
+# print("saikrish"[1])#a 
+# print("saikrish"[2])#i
+# print("saikrish"[3])#k
+# print("saikrish"[4])#r
+# print("saikrish"[5])#i
+# print("saikrish"[6])#s
+# print("saikrish"[7])#h
+# print("saikrish"[8])#
+
+# b="immutable"
+# print(b[2]+b[3]+b[4]+b[5]+b[6]+b[7]+b[8]) #mutable
+# print(b[4]+b[5:]) #table
+# print("saikrish"[-1])
+# print("saikrish"[-2])
+# print("saikrish"[-3])
+# print("saikrish"[-4])
+# print("saikrish"[-5])
+# print("saikrish"[-6])
+# print("saikrish"[-7])
+# print("saikrish"[-8])
+# print("saikrish"[-9])#IndexError: string index out of range
+
+s="saikrish"
+# print(s[0:0])
+# print(s[:])
+# print(s[:8])
+# print(s[:99])
+# print(s[-8:8])
+# print(s[0:1])
+# print(s[0:2])
+# print(s[0:3])
+# print(s[0:4])
+# print(s[0:5])
+# print(s[0:6])
+# print(s[0:7])
+# print(s[0:8])
+# print(s[0:9])
+# print(s[4:3])
+# print(s[5:3])
+# print(s[0:-1])#saikris
+# print(s[0:-2])#saikri
+# print(s[0:-3])#saikr
+# print(s[0:-4])#saik
+# print(s[0:-5])#sai
+# print(s[0:-6])#sa
+# print(s[0:-7])#s
+# print(s[0:-8])#blank line
+# print(s[0:-9])#balnk line
+
+v="saikrish"
+print(v[3:7])#kris
+print(v[4:6])#ri
+print(v[3:2])#blank line
+print(v[4:3])
+
+
+
+
+
+
