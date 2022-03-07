96d818b657ecb9beec994d8751f12fd079405ec9
saivenkat900 <99874195+saivenkat900@users.noreply.github.com>
2022-03-08 00:50:25 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,56 @@
+# name="hELLo pyThon"
+# print(name.capitalize())
+
+# name="hELLo pyThon"
+# print(name.casefold())
+# print(name.lower())
+
+# name="hELLo pyThon"
+# print(len(name))
+# print(name.center)
+# print(name.center(50,'*'))
+# print(name.center(100))
+
+# name="hELLo pyThon"
+# # print(type(name))
+# n=name.encode()
+# print(n)
+# print(n.decode)
+# print(type(b'hell0'))
+
+# name="hELLo pyThon"
+# print(name.upper())
+# print(name.lower())
+# print(name.islower())
+# b="CRICKET"
+# print(b.isupper())
+# c="saivenkat"
+# print(c.islower())
+
+# t="hELLo pyThon"
+# print(t.istitle())
+# print(t.capitalize())
+# print(t.title())
+# c="Hello python"
+# print(c.istitle())
+
+# p='hELLo pyThon'
+# print(p.startswith(''))
+# print(p.startswith(' '))
+# print(p.startswith('h'))
+
+# y=' hELLo pyThon'
+# print(y.startswith(''))
+# print(y.startswith(' '))
+# print(y.startswith('h'))
+
+# i='hELLo pyThon'
+# # print(i.endswith())
+# print(i.endswith(''))
+# print(i.endswith(' '))
+# print(i.endswith('h'))
+
+# b=''
+# n='  '
+# print(b.isspace())
+# print(n.isspace())
