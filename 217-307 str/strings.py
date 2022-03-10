2af8af16e69bdb0985756e72067a9db7ed156198
saivenkat900 <99874195+saivenkat900@users.noreply.github.com>
2022-03-11 00:34:08 +0530

    Add files via upload

---
+++ 217-307 str/strings.py
@@ -0,0 +1,42 @@
+# h="python course"
+# print(h.replace('course','programing'))
+# g="core python programing  core"
+# print(g.replace('core',2))
+# print(g.replace('core',str(2)))
+# print(g.replace('core','2'))
+# print(g.replace('o','k',1))
+# print(g.replace('o','k',2))
+# print(g.replace('o','k',3))
+# print(g.replace('o','k',4))
+# print(g.replace('o','k',5))
+
+# b="     python programming.     "
+# print(b.strip())
+# print(b.lstrip())
+# print(b.rstrip())
+
+# c="     python programming.     ".strip()
+# print(len(c))
+
+# d="hello","python",'pro'
+# print(d,type(d))
+# b=' world '.join(d)
+# print(b)
+# print("hello","python", "pro",sep="world")
+
+# a="hey"
+# b="*".join(a)
+# print(b)
+# e="hey"
+# d="**".join(e)
+# print(d)
+# f="hey",''
+# i="*".join(f)
+# print(i)
+
+# f="python programming"
+# print(f.removeprefix('p'))
+# print(f.removeprefix('P'))
+# print(f.removeprefix('py'))
+# print(f.removesuffix('ng'))
+# print(f.removesuffix('ming'))
