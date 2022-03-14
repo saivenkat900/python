31cbb5ef60496ca5e4bcc1edb4846593f8ec515f
saivenkat900 <99874195+saivenkat900@users.noreply.github.com>
2022-03-15 00:39:23 +0530

    Add files via upload

---
+++ 309-422 str/chr-translate.py
@@ -0,0 +1,104 @@
+# a=chr(0)
+# b=chr(32)
+# print(b.isspace())
+# print(a.isspace())
+# print("hello"+chr(32)+"pyhton")
+# print("hello"+' '+"python")
+# print("hello","python",sep=' ')
+# print("hello","python")
+
+# print(chr(32)+"hey") #one space hey
+# print(chr(0)+"hey") #hey
+
+# print(ord('A'))
+# print(ord('z'))
+# print(ord('A'))
+# print(ord('Z'))
+# print(ord('0'))
+# print(ord(' '))
+# print(ord('!'))
+
+# print(chr('o'))
+# print(chr(97))
+# print(chr(65))
+# print(chr(31))
+# print(chr(32))
+# print(chr(32).isspace())
+
+# print(ord("a"))
+# print(ord("z"))
+# print(ord("A"))
+# print(ord("Z"))
+# print(ord("_"))
+# print(ord("\t"))
+# print(ord("0"))
+# print(ord("10"))
+# print(ord(''))
+
+# v=bin(5)
+# print(v,type(v))
+# h=0b101
+# print(h,type(h))
+# j=0B101
+# print(j,type(j))
+
+# print(bin(23))
+# print(bin(24))
+
+# n=oct(11)
+# print(n,type(n))
+
+# a=hex(12)
+# print(a,type(a))
+# print(0x12)
+# print(0xA)
+# print(0xD)
+# print(0xF)
+# print(0xf)
+
+# p="pyTHon proGRAMming"
+# print(p.swapcase())
+
+# print("buy this forn Rs.{ed} or for Rs.{d}".format(d=11,ed=50))
+
+# print("hello\tpyhton")
+# print("hello \tpyhton")
+# print("hello\t pyhton")
+# print("\tpyhton")
+
+# a='R\tS\tV'
+# print(a.expandtabs(0))
+# print(a.expandtabs(1))
+# print(a.expandtabs(2))
+# print(a.expandtabs(3))
+# print(a.expandtabs(4))
+# print(a.expandtabs(5))
+# print(a.expandtabs(6))
+# print(a.expandtabs(7))
+# print(a.expandtabs(8))
+# print(a.expandtabs())
+# print(a.expandtabs(-1))
+
+# v='hello','python','core'
+# n='python'
+# print('-'.join(v))
+# print('*'.join(n))
+# print('*'.join("hello"))
+
+# r="hello python"
+# a=r.maketrans("l","u")
+# print(r.translate(a))
+
+# r={}
+# print(r,type(r))
+# v={1:"hello",2:"python"}
+# print(type(v))
+# print(v[3])
+
+
+
+
+
+
+
+
