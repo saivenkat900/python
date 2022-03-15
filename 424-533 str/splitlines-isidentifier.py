f5876fc77ba61bd528f92999e0504b407013e8e5
saivenkat900 <99874195+saivenkat900@users.noreply.github.com>
2022-03-15 23:56:43 +0530

    Add files via upload

---
+++ 424-533 str/splitlines-isidentifier.py
@@ -0,0 +1,32 @@
+# a="hello\ncore python\nprograming"
+# print(a.index('c'))
+# print(a.index('n'))
+# print(a[6:17])
+# print(a)
+# g=a.splitlines()
+# print(g[0])
+# print(g[1])
+# print(g[2])
+# print(g[3]) #index error:list index out of range
+
+# b="hello welcome to my world"
+# c=b.split()
+# print(b,type(b))
+# print(c[0][:5])
+# print(b.split(" "))
+# print(b.split(""))
+
+# a=5.6,7
+# print(a)
+
+# a=[int(x) for x in input().split()]
+# print(a,type(a))
+ 
+# b=6.9
+# print(b,type(b))
+
+# a="hello hi this is sai"
+# print(a.split())
+
+# b="hello hi this is sai"
+# print(b.split(":"))
