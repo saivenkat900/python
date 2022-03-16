3efd5b1c1124cc8a63a065968f4cf59536a4ee2d
saivenkat900 <99874195+saivenkat900@users.noreply.github.com>
2022-03-16 18:19:12 +0530

    Add files via upload

---
+++ strings methods/splitlines-isidentifier.py
@@ -0,0 +1,105 @@
+# a="hello\ncore python\nprograming"
+# print(a.index('c'))
+# print(a.index('n'))
+# print(a[6:17])
+# print(a)
+# g=a.splitlines()
+# print(g[0])
+# print(g[1])
+# print(g[2])
+# print(g[3]) #index error:list index out of range
+
+# b="hello welcome to my world"
+# c=b.split()
+# print(b,type(b))
+# print(c[0][:5])
+# print(b.split(" "))
+# print(b.split(""))
+
+# a=5.6,7
+# print(a)
+
+# a=[int(x) for x in input().split()]
+# print(a,type(a))
+ 
+# b=6.9
+# print(b,type(b))
+
+# a="hello hi this is sai"
+# print(a.split())
+
+# b="hello hi this is sai"
+# print(b.split(":"))
+
+# n="python#c#java#javascript"
+# print(n.split("#"))
+# print(n.split("#",0))
+# print(n.split("#",1))
+# print(n.split("#",2))
+# print(n.split("#",3))
+# print(n.split("#",4)
+
+# v=r"hello\bprogramming"
+# print(v,type(v))
+
+# y="hello!\nwelcome to \nmyworld"
+# print(y.splitlines())
+
+# y="hello!\nwelcome to \nmyworld"
+# print(y.splitlines(True))
+# print(y.splitlines(False))
+
+# a="hello sai"
+# print(a.zfill(20))
+
+# b="hello! how are you doing?"
+# print(b.partition("are"))
+# print(b.partition("you"))
+# print(b.partition("doing"))
+# c=b.partition("you")
+# print(c)
+# print(c[2])
+
+# m="we are learning python programming ,python is basic need  \
+#     for Ml,DS,AI,Dl,bigdata..etc"
+# print(m.partition("python"))
+# print(m.partition("are"))
+
+# c="python"
+# print(c.ljust(8),"programming basics")
+
+# d="python"
+# print(d.rjust(8),"programming basics")
+
+# z="python"
+# print(z.rjust(10),'R')
+# print(z.rjust(10,'R'))
+# print(z.ljust(10,'R'))
+
+# d="hello,hi,ji"
+# a=d.rsplit(" ", 1)
+# b=d.rsplit(": ", 1)
+# c=d.split(' ', 1)
+# print(a)
+# print(b)
+# print(c)
+
+# a='_hello'
+# print(a.isidentifier())
+# b='hello'
+# print(b.isidentifier())
+# c='Hello'
+# print(c.isidentifier())
+# d='_'
+# print(d.isidentifier())
+# e='_32'
+# print(e.isidentifier())
+# f='1abc'
+# print(f.isidentifier())
+# g='.1abc'
+# print(g.isidentifier())
+# h='1a bc'
+# print(h.isidentifier())
+
+
+
